88a9aa7a4acb823bce0f28e554b09210a86207ec
Yash Kumar Verma <yk.verma2000@gmail.com>
2019-06-18 11:20:42 +0530

    tracking code

--- generateJSON.php
+++ generateJSON.php
@@ -33,6 +33,16 @@ closedir($dir);
 <html>
 <head>
     <title>Global top 50: English</title>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142182602-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-142182602-3');
+</script>
+
 </head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <body>
