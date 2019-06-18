3eb2a7a458cce521a33daabb13005c17292eeeaa
Yash Kumar Verma <yk.verma2000@gmail.com>
2019-06-18 10:31:08 +0530

    test 3

--- generateJSON.php
+++ generateJSON.php
@@ -4,7 +4,7 @@ $path = "./downloads";
 $dir = opendir($path);
 $songs = [];
 
-$i=0;
+$i=1;
 while (($file = readdir($dir)) !== false) {
     if($file != "." && $file != ".." && $file!='index.html' && $file!= 'songs.json') {
 
@@ -23,11 +23,47 @@ while (($file = readdir($dir)) !== false) {
 	// show
     	array_push($songs, ['id'=>$i,'url'=>$newName, 'name'=>$file]);
         $i++;
-
-	// write to json
-    	print(json_encode($songs));
     }
 }
 
 closedir($dir);
 ?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Global top 50: English</title>
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<body>
+
+<div class="jumbotron">
+  <h1 class="display-4">Global Top 50 : English</h1>
+  <p class="lead"><i><a href="https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF" target="_BLANK">Playlist on Spotify</a></i></p>
+  <hr class="my-4">
+  <p><span style="color:red"><b>No</b></span> PopUp! <span style="color:red"><b>No</b></span> Ads! <span style="color:red"><b>No</b></span> Mess! <span style="color:red"><b>NO BS !!!</b></span></p>
+  <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> -->
+</div>
+
+<div class="container-fluid">
+    <table class="table table-striped table-hover table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <!-- <th scope="col">Download</th> -->
+    </tr>
+  </thead>
+  <tbody>
+<?php foreach ($songs as $song) { ?>
+    <tr>
+        <td><?php echo $song['id']; ?></td>
+        <!-- <td><?php echo $song['name']; ?></td> -->
+        <td><a href="<?php echo $song['url']; ?>" class="btn btn-light"><?php echo $song['url']; ?></a></td>
+    </tr>
+<?php } ?>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
